07a3370677643cf68ffa27eb6f42236bb0f0dd39
Monsse <166641982+Monsse28@users.noreply.github.com>
2025-11-26 11:23:12 -0600

    Add files via upload

---
+++ operaciones.py
@@ -0,0 +1,29 @@
+# Funcion que recibe una lista y regresa la sumatoria
+def sumar(numeros:list[float]) -> float:
+    res=sum(numeros)
+    return res
+
+# Funcion que recibe una lista y regresa su promedio
+def promedio(numeros:list[float]) ->float:
+    if( len(numeros)==0):
+        return 0
+    prom=sum(numeros) / len(numeros)
+    return prom
+
+# Contar las organizaciones en el archivo CSV
+import pandas as pd
+def contador_org(ruta):
+    datos=pd.read_csv(ruta, encoding="latin-1")
+    conteo= len(datos)
+    return conteo
+
+datos=[10, 20, 1.5, 70.2]
+s = sumar(datos)
+print("Sumatoria " , s)
+
+p=promedio(datos)
+print("Promedio " , p)
+
+cant=contador_org("Organization_v3.csv")
+print("Cantidad de organizaciones " ,cant)
+
---
+++ test_operaciones.py
@@ -0,0 +1,16 @@
+#pruebas unitarias de la funcion "sumar"
+import pytest 
+from operaciones import *
+
+def test_sumar1():
+    res=sumar([1,2,3] )
+    assert res==6
+
+def test_sumar2():
+    res=sumar([10, 15, 20] )
+    assert res==45
+
+def test_sumar3():
+    res=sumar([ ] )
+    assert res==0
+
